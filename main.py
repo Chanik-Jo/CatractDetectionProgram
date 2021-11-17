7567e80dc7fdfe827fa9ad86c1188bf968bb2bf4
Chanik-Jo <jochanig@gmail.com>
2021-11-17 16:37:19 +0900

    눈알 찍힘 멀티쓰레드 포기!

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #ignore warnings
 class camThread(QThread):
 
     changePixmap = pyqtSignal(QImage)
+    send_instance_singal = pyqtSignal("PyQt_PyObject")
+    eyeList=[]
 
 
     def __init__(self,parent):
         super().__init__(parent)
         self.parent = parent
-
+        self.camera = cv2.VideoCapture(0)
 
 
 
@@ -31,16 +33,16 @@ class camThread(QThread):
 
         while run_video:
             print("run_video cam on  (thread 1) " )
-            self.parent.mutex.lock()
-            camera = cv2.VideoCapture(0)
-            self.parent.mutex.unlock()
-            ret, image = camera.read()
+
+            ret, image = self.camera.read()
 
             height, width = image.shape[:2]  # 가로세로 해상도.
 
             if ret == False:  # 카메라가 정상자동하지 않을시.
                 run_video = False
             print("cam read  (thread 1) ")
+
+
             color_swapped_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # cvtColor=convertColor  rgb type.
             '''
                         웹캠 정상작동여부 테스트
@@ -63,54 +65,44 @@ class camThread(QThread):
 
             resizeImage = qt_image1.scaled(videoWidth, videoHeight)
             print("shoot cam!! (thread 1) ")
-            self.changePixmap.emit(resizeImage)
+            self.changePixmap.emit(resizeImage)#전체 얼굴 프린트.
+            print("emit하면 다음으로 넘어가긴 하나? ")
 
-
-class eyesThread(QThread):
-    send_instance_singal = pyqtSignal("PyQt_PyObject")
-    eyeList=[]
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.parent = parent
-
-
-    def run(self):
-        while (True):
-            self.parent.mutex.lock()
-            self.cap = cv2.VideoCapture(0)
-            self.parent.mutex.unlock()
-            ret, img = self.cap.read()
-
-
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            #얼굴과 눈알 계산하기.
+            img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)#camera.read 의 image
             faces = detector.detectMultiScale(img, 1.3, 5)
             print("face detect  (thread2)")
             for (x, y, w, h) in faces:  # 결과값 좌표. 좌상 xy 와 높이 너비 얼굴 이미지는 1개여서 루프는 1회만 돌아간다.
                 # 생각해보니 rgb가 아닌 bgr순서였다. 이건 파란색 네모.
-                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)#얼굴에 파란네모 씌우기.
                 roi_color = img[y:y + h, x:x + w]  # 풀컬러 얼굴 범위한정 이미지.
                 eyes = eye_cascade.detectMultiScale(roi_color, minNeighbors=15)
                 print("eye detect  (thread2) ")
                 for (ex, ey, ew, eh) in eyes:  # 눈알은 2개니 루프는 2회.
                     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
                     eyeImgReturn = roi_color[ey:ey + eh, ex:ex + ew, :]  # 배열 컷  가로세로가 상당히 헷갈린다 주의주의.
-                    #eyeImgReturn = cv2.cvtColor(eyeImgReturn, cv2.COLOR_BGR2RGB)
-
-                    image = PIL.Image.fromarray(eyeImgReturn, 'RGB') # image.show()
-                    image.show()  # 눈깔따기 알고리즘 완성.
+                    # eyeImgReturn = cv2.cvtColor(eyeImgReturn, cv2.COLOR_BGR2RGB)  불필요한 코드.
+                    print("eye image arr shape", str(np.shape(eyeImgReturn)))
+                    image = PIL.Image.fromarray(eyeImgReturn, 'RGB')  # image.show()
+                    #image.show()  # 눈깔따기 알고리즘 완성.
                     image = ImageQt(image).copy()
-                    print("eyeImage  (thread2) ",image)
+                    print("eyeImage  (thread2) ", image)
+                    # 이시점에서 image를 qImage로 바꾸어야한다.
+
+                    #h, w, _ = eyeImgReturn.shape
+
+                    #qimage = QImage(eyeImgReturn.data, w, h, 3 * w, QImage.Format_RGB888)
+                    # 줄당 바이트 갯수는 어짜피 3원색 *가로줄 전체 일테니...
+                    #print("qimage print ",qimage)
                     self.eyeList.append(image)
+                    print(self.eyeList)
 
-
-            print("(thread2) eyeList : ",self.eyeList)
-            if (len(self.eyeList)<=1):#눈알이 양쪽 다. 없으면 명령실행 하지 않는다.
+            print("눈알 인식된 이미지 갯수",len(self.eyeList))
+            if(len(self.eyeList)<=1):
+                print("양쪽눈이 인식되지 않았습니다. ")
                 continue
+            print("emit 준비.")
             self.send_instance_singal.emit(self.eyeList)
-            self.eyeList.clear()#그다음 다시 새로운 얼굴.
-
-
-
 
 
 
@@ -131,10 +123,10 @@ class WindowClass(QMainWindow, form_class):
         self.resultTxtWidget.setText("테스트용 문자.")
         self.show()
 
-        x = camThread(self) ; y = eyesThread(self) #이런식으로 만들면 공식문서왈 이 스레드내부에서 동작한다고.
-        x.changePixmap.connect(self.webCamOn) ; y.send_instance_singal.connect(self.dsplyEyes)
-        x.start() ; y.start()
-
+        x = camThread(self)
+        x.changePixmap.connect(self.webCamOn)
+        x.send_instance_singal.connect(self.dsplyEyes)
+        x.start()
 
 
 
@@ -145,15 +137,11 @@ class WindowClass(QMainWindow, form_class):
 
     @pyqtSlot("PyQt_PyObject")
     def dsplyEyes(self,eyeList):
-        if(len(self.eyeImages)==0):#이미지가 들어오지 않은경우 심지어 이전사람 이미지도 없을때.
-            pass
-        else:
-
-            self.eyeLeftWidget.setPixmap(QPixmap.fromImage(eyeList[0]))
-            self.eyeRightWidget.setPixmap(QPixmap.fromImage(eyeList[1]))
 
 
-
+        self.eyeLeftWidget.setPixmap(QPixmap.fromImage(eyeList[0]))
+        self.eyeRightWidget.setPixmap(QPixmap.fromImage(eyeList[1]))
+        eyeList.clear()  # 그다음 다시 새로운 얼굴.
 
 
 

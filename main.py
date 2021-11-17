5a3b477afd7b65d3725ce878d673b32ba01dec84
Chanik-Jo <jochanig@gmail.com>
2021-11-17 19:43:43 +0900

    눈알 찍힘 멀티쓰레드 포기!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2,os,sys,PIL
 import numpy as np
+from threading import Thread
 from PIL.ImageQt import ImageQt
 from keras.models import load_model
 from PyQt5 import QtGui, uic, QtCore
@@ -103,6 +104,7 @@ class camThread(QThread):
             videoWidth = self.parent.videoWidget.width()
             videoHeight = self.parent.videoWidget.height()
             eyefaceImage = eyefaceImage.scaled(videoWidth, videoHeight)
+            #전체 얼굴 해상도를 줄이는게 나중에있으니 눈알 해상도와는 관계 없음.
             self.changePixmap.emit(eyefaceImage)
             print("눈알 인식된 이미지 갯수",len(self.eyeList))
             if(len(self.eyeList)<=1):
@@ -127,7 +129,7 @@ class WindowClass(QMainWindow, form_class):
         # eyeLeft
         # eyeRight
         #self.videoWidget.setStyleSheet("background-color:rgb(255, 0, 255);")
-        self.resultTxtWidget.setText("테스트용 문자.")
+        self.resultTxtWidget.setText("검사검사")
         self.show()
 
         x = camThread(self)
@@ -144,6 +146,11 @@ class WindowClass(QMainWindow, form_class):
 
     @pyqtSlot("PyQt_PyObject")
     def dsplyEyes(self,eyeList):
+        if(len(eyeList)==2):
+            thread = Thread(target=self.writePerdiction, args=(eyeList,))
+            print("thread call")
+            thread.start()
+
         #이미지 크기 위젯에 일치시키기.
         leftWidgetWidth=self.eyeLeftWidget.width()
         leftWidgetHeight=self.eyeLeftWidget.width()
@@ -157,6 +164,46 @@ class WindowClass(QMainWindow, form_class):
         self.eyeLeftWidget.setPixmap(QPixmap.fromImage(eyeList[0]))
         self.eyeRightWidget.setPixmap(QPixmap.fromImage(eyeList[1]))
         eyeList.clear()  # 그다음 다시 새로운 얼굴.
+        #확률 계산작업은 부하가 걸려서 사진이 못 올라올것을 의심하여, 별도의 스레드로 분리하게됨.
+
+
+
+    def writePerdiction(self,eyeList):
+        print("thread working")
+        resultPrediction = np.ndarray(shape=(2,128,128,3),dtype=np.float32)
+
+        incomingImage1 = eyeList[0].convertToFormat(QtGui.QImage.Format.Format_RGB32)
+        incomingImage2 = eyeList[1].convertToFormat(QtGui.QImage.Format.Format_RGB32)
+
+        width = incomingImage1.width()
+        height = incomingImage1.height()
+
+        ptr = incomingImage1.constBits()
+        resultPrediction[0] = np.array(ptr).reshape(height, width, 3)
+
+        width = incomingImage2.width()
+        height = incomingImage2.height()
+
+        ptr = incomingImage2.constBits()
+        resultPrediction[1] = np.array(ptr).reshape(height, width, 3)
+
+
+
+        prediction = catractModel.predict(resultPrediction)
+
+        _,a=prediction[0]
+        _,b=prediction[1]
+
+        self.leftEyeWidgetResult.setText(a)
+        self.rightEyeWidgetResult.setText(b)
+
+
+
+
+
+
+
+
 
 
 
